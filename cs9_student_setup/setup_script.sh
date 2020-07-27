e94173fc8f67c6de91d8b194d3c417d4c3fe221e
jacob wolf <jacob.h.wolf@gmail.com>
2020-07-27 20:09:00 +0800

    Rewrite to address year 1 bugs

--- cs9_student_setup/setup_script.sh
+++ cs9_student_setup/setup_script.sh
@@ -6,171 +6,196 @@ BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 PURPLE='\033[1;35m'
 NC='\033[0m'
+CLEAR_LINE='\r\033[K'
+
+
+# Exit if any subcommand fails
+#set -e
+
+function set_ownership {
+    printf "${CLEAR_LINE} 🔍 ${BLUE}Giving user write permission local directory..."
+    #changing ownership of /usr/local/etc directories
+    sudo chown -R $(whoami) /usr/local/etc
+    #giving user write permission
+    chmod u+w /usr/local/etc
+}
+
+function macos_version_check {
+    # macOS version check, Hombrew require 10.12 or higher
+    printf "${CLEAR_LINE} 🔍 ${BLUE}Checking macOS version...${NC}"
+    os_version=$( defaults read loginwindow SystemVersionStampAsString )
+    vercomp $os_version $1 
+    if [[ $? == 2 ]]; then
+        printf "${CLEAR_LINE}${RED}You are running macOS verion %s.\n" $os_version
+        printf "${RED}Installing Homebrew requires macOS version $1 or higher. Please ask an instructor for help. ${NC}"
+        read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+        printf "🔍 ${BLUE}Checking system requiremets...${NC}"
+    fi
+}
+
+function install_xcode  {
+    printf "${CLEAR_LINE}${BLUE}Installing Xcode... (this may take a while)${NC}"
+    xcode_version=$( xcode-select --version )
+    IFS=' ' # space is set as delimiter
+    read -ra ADDR <<< "$xcode_version" #parse version number from response
+    vercomp ${ADDR[${#ADDR[@]}-1]} $1 
+    if [[ $? == 2 ]]; then
+        xcode-select --install > /dev/null
+        xcode_version=$( xcode-select --version )
+        IFS=' ' # space is set as delimiter
+        read -ra ADDR <<< "$xcode_version" #parse version number from response
+        vercomp ${ADDR[${#ADDR[@]}-1]} $1 
+        if [[ $? == 2 ]]; then
+            printf "${RED}Message from version request: %s${NC}\n" "$xcode_version"
+            printf "${RED}Unexpected output from Xcode installation. Please ask an instructor for help. ${NC}"
+            read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+            printf "💻 ${BLUE}Installing xcode command line tools...${NC}"
+        fi
+    fi
+}
+
+function install_homebrew {
+    # Homebrew installation
+    printf "${CLEAR_LINE}${BLUE}Installing Homebrew... (this may take a while)${NC}"
+    brew_version=$( brew --version )
+    IFS='\n' # new line is set as delimiter
+    read -ra ADDR <<< "$brew_version" #parse version line from response
+    IFS=' '
+    read -ra ADDR1 <<< ${ADDR[0]} #parse version number from top line
+    vercomp ${ADDR1[${#ADDR1[@]}-1]} $1 
+    if [[ $? == 2 ]]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null
+        brew_version=$( brew --version )
+        IFS='\n' # new line is set as delimiter
+        read -ra ADDR <<< "$brew_version" #parse version line from response
+        IFS=' '
+        read -ra ADDR1 <<< ${ADDR[0]} #parse version number from top line
+        vercomp ${ADDR1[${#ADDR1[@]}-1]} $1 
+        if [[ $? == 2 ]]; then
+            printf "${RED}Output from version request: %s${NC}\n" "$brew_version"
+            printf "${RED}Unexpected output from Homebrew installation. Please ask an instructor for help. ${NC}"
+            exit 1
+        fi
+    fi
+}
+
+
+function install_brew_package {
+    printf "${CLEAR_LINE}🔨 ${BLUE}Installing $1...${NC}"
+    if ! command -v $1 > /dev/null; then
+        if [[ -n $3 ]]; then
+            brew cask install &> /dev/null
+        else
+            brew install $1 &> /dev/null
+        fi
+    fi
+    version=$( $1 --version )
+    IFS=' ' # space is set as delimiter
+    read -ra ADDR <<< "$version" #parse version number from response
+    vercomp ${ADDR[${#ADDR[@]}-1]} $2 
+    if [[ $? == 2 ]]; then
+        if [[ -n $3 ]]; then
+            brew cask upgrade $1 &> /dev/null
+        else
+            brew upgrade $1 &> /dev/null
+        fi
+        version=$( $1 --version )
+        IFS=' ' # space is set as delimiter
+        read -ra ADDR <<< "$version" #parse version number from response
+        vercomp ${ADDR[${#ADDR[@]}-1]} $2 
+        if [[ $? == 2 ]]; then
+            printf "${CLEAR_LINE}${YELLOW}Output from version request: %s${NC}\n" "$version"
+            printf "${YELLOW}Expected version $2 or higher. Please ask an instructor for help. ${NC}\n"
+            read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+            printf "🔨 ${BLUE}Installing $1....${NC}"
+        fi
+    fi
+}
 
 # file system creation
-printf "${BLUE}Creating file system...${NC}\n" sudo chown -R $(whoami) /usr/local/etc
-cd /Users/`whoami`/Desktop
-mkdir -p cs9
-cd cs9
-mkdir -p unit_00
-printf "${GREEN}✓✓✓ File system created!${NC}\n\n\n"
+function filesys {
+    printf "${CLEAR_LINE}${BLUE}Creating file system...${NC}"
+    DIR=/Users/`whoami`/Desktop/cs9/unit_00
+    if [ ! -d "$DIR" ]; then
+        mkdir -p $DIR
 
-#changing ownership of /usr/local/etc directories
-sudo chown -R $(whoami) /usr/local/etc
-
-#giving user write permission
-chmod u+w /usr/local/etc
-
-printf "${BLUE}Installing Xcode...${NC}\n"
-xcode-select --install
-xcode_version=$( xcode-select --version )
-if [[ "$xcode_version" =~ "xcode-select version "[2-9][0-9][0-9][0-9]"." ]]; then
-  printf "${GREEN}✓✓✓ Xcode installed!${NC}\n\n\n"
-else
-  printf "${RED}Message from version request: %s${NC}\n" "$xcode_version"
-  printf "${RED}Unexpected output from Xcode installation. Please ask an instructor for help. ${NC}"
-  read -p "Proceed? [y/n]: " ans
-  if [[ $ans == 'n' ]]; then
-    exit 1
-  fi
-fi
-
-# macOS version check, Hombrew require 10.12 or higher
-printf "${BLUE}Checking macOS version...${NC}\n"
-os_version=$( defaults read loginwindow SystemVersionStampAsString )
-printf "You are running macOS verion %s.\n" $os_version
-if [[ "$os_version" =~ 10\.1[2-9]\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ macOS version supported by Homebrew!${NC}\n\n\n"
-else
-  printf "${RED}Installing Homebrew requires macOS version 10.12 or higher. Please ask an instructor for help. ${NC}"
-  read -p "Proceed? [y/n]: " ans
-  if [[ $ans == 'n' ]]; then
-    exit 1
-  fi
-fi
-
-# Homebrew installation
-printf "${BLUE}Installing Homebrew...${NC}\n"
-brew_version=$( brew --version )
-if [[ "$brew_version" =~ "Homebrew "2\.[1-9]\.[1-9][0-9]?.? ]]; then
-  printf "${GREEN}✓✓✓ Homebrew already installed !${NC}\n\n\n"
-else
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew_version=$( brew --version )
-  if [[ "$brew_version" =~ "Homebrew "2\.[1-9]\.[1-9][0-9]?.? ]]; then
-    printf "${GREEN}✓✓✓ Homebrew installed !${NC}\n\n\n"
-  else
-    printf "${RED}Output from version request: %s${NC}\n" "$brew_version"
-    printf "${RED}Unexpected output from Homebrew installation. Please ask an instructor for help. ${NC}"
-    read -p "Proceed? [y/n]: " ans
-    if [[ $ans == 'n' ]]; then
-      exit 1
     fi
-  fi
-fi
+}
 
-# python3 installation
-printf "${BLUE}Installing python3...${NC}\n"
-python3_version=$( python3 --version )
-if [[ "$python3_version" =~ "Python "3\.[0-9]\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ Python3 already installed!${NC}\n\n\n"
-else
-  brew install python3
-  python3_version=$( python3 --version )
-  if [[ "$python3_version" =~ "Python "3\.[0-9]\.[0-9]? ]]; then
-    printf "${GREEN}✓✓✓ Python3 installed!${NC}\n\n\n"
-  else
-    printf "${RED}Output from version request: %s${NC}\n" "$python3_version"
-    printf "${RED}Unexpected output from Python3 installation. Please ask an instructor for help. ${NC}"
-    read -p "Proceed? [y/n]: " ans
-    if [[ $ans == 'n' ]]; then
-      exit 1
+function setup_venv {
+    # Setting up virtual environment
+    printf "${CLEAR_LINE}${BLUE}Creating virtual environment...${NC}"
+    DIR=/Users/`whoami`/Desktop/cs9
+    cd $DIR
+    python3 -m venv env
+    printf 'PATH_add env/bin' > .envrc
+    FILE=/Users/`whoami`/.bash_profile
+    if [ ! -e $FILE ]; then
+        printf 'eval "$(direnv hook bash)"' > $FILE
+    else
+        if ! grep -q 'eval "$(direnv hook bash)"' "$FILE"; then
+            printf '\neval "$(direnv hook bash)"' >> $FILE
+        fi
     fi
-  fi
-fi
+    source ~/.bash_profile
+    direnv allow .
+}
 
-# git installation
-printf "${BLUE}Installing git...${NC}\n"
-git_version=$( git --version )
-if [[ "$git_version" =~ "git version "2\.[2-9][0-9]?\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ git already installed!${NC}\n\n\n"
-else
-  brew install git
-  git_version=$( git --version )
-  if [[ "$git_version" =~ "git version "2\.[2-9][0-9]?\.[0-9]? ]]; then
-    printf "${GREEN}✓✓✓ git installed!${NC}\n\n\n"
-  else
-    printf "${RED}Output from version request: %s${NC}\n" "$git_version"
-    printf "${RED}Unexpected output from git installation. Please ask an instructor for help. ${NC}"
-    read -p "Proceed? [y/n]: " ans
-    if [[ $ans == 'n' ]]; then
-      exit 1
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
     fi
-  fi
-fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
 
-# direnv installation
-printf "${BLUE}Installing direnv...${NC}\n"
-direnv_version=$( direnv --version )
-if [[ "$direnv_version" =~ 2\.[2-9][0-9]?\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ direnv already installed!${NC}\n\n\n"
-else
-  brew install direnv
-  direnv_version=$( direnv --version )
-  if [[ "$direnv_version" =~ 2\.[2-9][0-9]?\.[0-9]? ]]; then
-    printf "${GREEN}✓✓✓ direnv installed!${NC}\n\n\n"
-  else
-    printf "${RED}Output from version request: %s${NC}\n" "$direnv_version"
-    printf "${RED}Unexpected output from direnv installation. Please ask an instructor for help. ${NC}"
-    read -p "Proceed? [y/n]: " ans
-    if [[ $ans == 'n' ]]; then
-      exit 1
-    fi
-  fi
-fi
+printf "🔍 ${BLUES}Checking system requirements..."
+set_ownership
+macos_version_check 10.12
+printf "${CLEAR_LINE}✔️  ${GREEN}System requirements passed!${NC}\n"
 
-# atom installation
-printf "${BLUE}Installing Atom...${NC}\n"
-atom_version=$( atom --version )
-if [[ "$atom_version" =~ 1\.[2-9][0-9]?\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ atom already installed!${NC}\n\n\n"
-else
-  brew cask install atom
-  atom_version=$( atom --version )
-  if [[ "$atom_version" =~ 1\.[2-9][0-9]?\.[0-9]? ]]; then
-    printf "${GREEN}✓✓✓ atom installed!${NC}\n\n\n"
-  else
-    printf "${RED}Output from version request: %s${NC}\n" "$atom_version"
-    printf "${RED}Unexpected output from atom installation. Please ask an instructor for help. ${NC}"
-    read -p "Proceed? [y/n]: " ans
-    if [[ $ans == 'n' ]]; then
-      exit 1
-    fi
-  fi
-fi
+printf "💻 ${BLUE}Installing Xcode command line tools...${NC}"
+install_xcode 2354
+printf "${CLEAR_LINE}✔️  ${GREEN}Xcode command line tools installed!${NC}\n"
 
-# Setting up virtual environment
-printf "${BLUE}Creating virtual environment...${NC}\n"
-python3 -m venv env
-printf 'PATH_add env/bin' > .envrc
-if [ ! -e /Users/`whoami`/.bash_profile ]; then
-    printf 'eval "$(direnv hook bash)"' > /Users/`whoami`/.bash_profile
-else
-    printf '\neval "$(direnv hook bash)"' >> /Users/`whoami`/.bash_profile
-fi
-source ~/.bash_profile
-direnv allow .
-python_version=$( python --version )
-if [[ "$python_version" =~ "Python "3\.[0-9]\.[0-9]? ]]; then
-  printf "${GREEN}✓✓✓ Virtual environment created and auto-activiation configured!${NC}\n\n\n"
-else
-  printf "${RED}Using: %s, virtual environment auto-activation may not be configured.${NC}\n" "$python_version"
-  printf "${RED}Please ask an instructor for help. ${NC}"
-  read -p "Proceed? [y/n]: " ans
-  if [[ $ans == 'n' ]]; then
-    exit 1
-  fi
-fi
+printf "🍺 ${BLUE}Installing Homebrew...${NC}"
+install_homebrew 2.4.8
+printf "${CLEAR_LINE}✔️  ${GREEN}Homebrew installed!${NC}\n"
+
+printf "🔨 ${BLUE}Installing brew packages...${NC}"
+install_brew_package python3 3.7.7
+install_brew_package direnv 2.20.2
+install_brew_package git 2.27
+install_brew_package atom 0 cask
+printf "${CLEAR_LINE}✔️  ${GREEN}Brew packages installed!${NC}\n"
+
+printf "🗂 ${BLUE}Setting up file environment for class...${NC}"
+filesys
+setup_venv
+printf "${CLEAR_LINE}✔️  ${GREEN}File environment created!${NC}\n"
 
 printf "${PURPLE}Your computer is configured! Please restart Terminal. ${NC}\n"
 exit 0
