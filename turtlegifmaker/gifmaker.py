e36813594c89ef1e3b9783aedc988a79031ebe8c
Jenny Han <jennyhan@Jennys-MacBook-Pro.local>
2020-12-10 14:57:39 -0800

    added comments and debugged path typo

--- turtlegifmaker/gifmaker.py
+++ turtlegifmaker/gifmaker.py
@@ -1,6 +1,6 @@
 from turtle import *
 import tkinter as _
-from project import main
+from animation import main # to be changed to a dynamic import statement
 import settings
 from PIL import ImageGrab, Image
 import os
@@ -13,7 +13,7 @@ FRAMES_PER_SECOND = 10
 image_list = []
 
 def grab_canvas():
-    "Grabs an Image of the screen"
+    "Returns an Image of the turtle screen, accounting for Mac Retina Pixel Doubling"
     canvas = getscreen().getcanvas()
     x0 = canvas.winfo_rootx() + canvas.winfo_x()
     y0 = canvas.winfo_rooty() + canvas.winfo_y()
@@ -22,28 +22,31 @@ def grab_canvas():
     return ImageGrab.grab(bbox=(x0*2 + 15, y0*2 + 15, x1*2 - 15, y1*2 -15)) #due to pixel doubling on Mac Retina Screen
 
 def draw(your_drawing_function):
+    "Executes the selected animation"
     your_drawing_function()
-    print("ok finished drawing!")
     stop()
 
 def stop():
+    "Flags the running variable to stop"
     global running
     running = False
 
 def record_screen():
+    "while the drawing is running, PIL to take screenshots and append to list"
     curr_image = grab_canvas()
     image_list.append(curr_image)
     if running:
         ontimer(record_screen, int(1000 / FRAMES_PER_SECOND))
 
 def makegif(your_drawing_function, gif_name):
+    "Takes the name of the animation function and a name for the gif file. Returns a gif of the animation in current directory."
     speed(0)
     hideturtle()
     record_screen()  # start the recording
     draw(your_drawing_function) # start drawing
-    path_name = os.getcwd() + gif_name + '.gif'
-    image_list[0].save(path_name, format='GIF',
-               save_all=True, append_images=image_list[1:], optimize=False, duration=40, loop=0)
+    path_name = os.getcwd() + '/' + gif_name + '.gif'
+    print(path_name)
+    image_list[0].save(path_name, format='GIF', save_all=True, append_images=image_list[1:], optimize=False, duration=40, loop=0)
 
 if __name__ == '__main__':
-    makegif(main, 'test');
+    makegif(main, 'test')
