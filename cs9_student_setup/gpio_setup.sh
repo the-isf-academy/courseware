02d32f2d78b8598cb5403c768af7985ce456aa58
emmaqbrown <eqbrown@isf.edu.hk>
2021-08-30 09:28:59 +0800

    init commit

---
+++ cs9_student_setup/gpio_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function setup_gpiozero {
+        printf "Updating Pi and installing GPIOZERO library\n"
+
+        sudo pip3 install gpiozero
+        sudo apt-get install rpi.gpio
+}
+
+setup_gpiozero
+
+prinf "\n"
+printf "Your Pi is configured!\n"
+~                                                                             
+~                                                                             
+~                                                                             
+~                                                                             
+~                                                                             
+~                              
