000264709fd58118d56101151f3a0e8d27e2bb12
Jacob Wolf <jacob.h.wolf@gmail.com>
2022-02-21 15:36:49 -0800

    Add datetime format options

--- personal_data_pull/whatsapp_export/export_whatsapp.py
+++ personal_data_pull/whatsapp_export/export_whatsapp.py
@@ -6,18 +6,19 @@ from pathlib import Path
 import tempfile
 import zipfile
 import pandas as pd
-
+from simple_term_menu import TerminalMenu
 
 DATA_DIR = "data"
 EXPORT_DIR = Path("./export")
 OUTFILE = "whatsapp.csv"
+DATETIME_FORMAT = "%m/%d/%y, %X"
 
 def get_chat_df(zipped_chat):
     with tempfile.TemporaryDirectory() as tmp_chat_dir:
         tmp_submission_dir = Path(tmp_chat_dir)
         with zipfile.ZipFile(zipped_chat, mode='r') as zip_ref:
             chat_file = (zipfile.Path(zip_ref) / "_chat.txt")
-            df = get_whatsapp_df(chat_file)
+            df = get_whatsapp_df(chat_file, DATETIME_FORMAT)
             chat_name = zip_ref.filename.split(" - ")[1].split(".")[0] 
             df['chat_name'] = chat_name
             return df
@@ -34,4 +35,8 @@ def export_whatsapp_to_csv():
     chat_dfs.to_csv(EXPORT_DIR / "whatsapp.csv")
 
 if __name__ == '__main__':
+    datetime_dict = {"DD/MM/YYYY": "%d/%m/%y, %X","MM/DD/YYYY": "%m/%d/%y, %X"}
+    date_format_menu = TerminalMenu(datetime_dict.keys(), title="Select date format of your chat data:")
+    menu_entry_index = date_format_menu.show()
+    DATETIME_FORMAT = datetime_dict[list(datetime_dict.keys())[menu_entry_index]]
     export_whatsapp_to_csv()
--- personal_data_pull/whatsapp_export/whatsapp_df.py
+++ personal_data_pull/whatsapp_export/whatsapp_df.py
@@ -5,12 +5,13 @@ from pathlib import Path
 from hashlib import sha1
 import yaml
 
-def get_whatsapp_df(chat_file):
+
+def get_whatsapp_df(chat_file, datetime_format="%m/%d/%y, %X"):
     with chat_file.open() as f:
         lines = f.readlines()
         parsed_lines = []
         for line in lines:
-           parsed_line = parse_line(line)
+           parsed_line = parse_line(line, datetime_format)
            if parsed_line:
                parsed_lines.append(parsed_line)
         df = pd.concat([pd.DataFrame({'date':[date], 'sender':[sender], 'message':[message]}) for date, sender, message in parsed_lines])
@@ -18,7 +19,7 @@ def get_whatsapp_df(chat_file):
     return df
 
 
-def parse_line(line):
+def parse_line(line, datetime_format):
     line = line.strip('\u200e')
     if line.count(":") >= 3:
         split_line = line.split(':', 3)
@@ -26,7 +27,7 @@ def parse_line(line):
         message = split_line[-1].strip().strip('\u200e')
         date, name = meta_data.split(']')
         date = date.strip('[')
-        date = datetime.strptime(date, '%m/%d/%y, %X')
+        date = datetime.strptime(date, datetime_format)
         name = name.strip()
         return (date, name, message)
     else:
