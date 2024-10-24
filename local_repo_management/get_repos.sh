29348417f0741e7143dee48ba885242e33ae29ad
Britte <britte.genzlinger@gmail.com>
2024-10-24 08:59:47 +0800

    update for new formatting and add a projects folder

--- local_repo_management/get_repos.sh
+++ local_repo_management/get_repos.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 FRONT_URL="https://github.com/the-isf-academy/"
-LAB_PREFIX="$2"
+LAB_PREFIX="$2_"
 BACK_URL=".git"
+FOLDER="./~/Desktop/"
+mkdir projects
 grep . $1 | while read LINE ; do 
     #Strip new line from LINE
-    #echo "$FRONT_URL$LAB_PREFIX-${LINE//[$'\t\r\n ']}$BACK_URL"
-    git clone "$FRONT_URL$LAB_PREFIX-${LINE//[$'\t\r\n ']}$BACK_URL"
+    # echo "$FRONT_URL$LAB_PREFIX${LINE//[$'\t\r\n ']}$BACK_URL" $FOLDER$LAB_PREFIX${LINE//[$'\t\r\n ']}
+    cd projects
+    git clone "$FRONT_URL$LAB_PREFIX${LINE//[$'\t\r\n ']}$BACK_URL"
+    cd ..
 done
